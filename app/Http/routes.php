7582bea62be77e914ab613a98422215d7830eced
TraianAlex <victor_traian@yahoo.com>
2015-12-28 14:21:47 -0500

    auth scaffolding in 5.2, add helpers from 5.1, api controller, assets in  public

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,8 +24,12 @@ Route::get('/', function () {
 | it contains. The "web" middleware group is defined in your HTTP
 | kernel and includes session state, CSRF protection, and more.
 |
+| Route::resource() -> resource() Laravel 5.2
+| manual copied from 5.1 in illuminate\fundation\helpers.php line 736
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+    resource('api', 'ApiController');
+    Route::get('/home', 'HomeController@index');
 });
